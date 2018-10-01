c5102aa9c6ce2e2caad202885ff02668792bd52a
Angela Larryant <angelarryant@gmail.com>
2018-10-01 18:35:49 +0000

    Buffered chunks and printed all at the end

---
+++ step2/printing-with-buffered.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+    /* Add your code here */
+
+  https.get(requestOptions, function (response) {
+  response.setEncoding('utf8');
+
+  var dataString = '';
+  response.on('data', function (chunk) {
+
+    dataString += chunk;
+    });
+
+  response.on('end', function() {
+    console.log(dataString);
+    });
+  });
+};
+
+
+
+console.log(getAndPrintHTML());
+
