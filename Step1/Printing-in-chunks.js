4c3db659e14c8156391b5c48242dcf22a040b9ea
Angela Larryant <angelarryant@gmail.com>
2018-10-01 18:09:46 +0000

    Completed Step 1, printing in chunks

---
+++ Step1/Printing-in-chunks.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+https.get(requestOptions, function (response) {
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log('\n', data);
+  });
+
+  // response.on('end', function() {
+  //   console.log('');
+  // });
+});
+
+}
+
+console.log(getAndPrintHTMLChunks());
